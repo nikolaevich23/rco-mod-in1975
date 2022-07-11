d76dc18fc3bb47cea399e00b04016058201ff3ad
nikolaevich23 <in1975@ya.ru>
2022-07-11 20:56:28 +0400

    update

--- extr.bat
+++ extr.bat
@@ -1,8 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 set pt=Rcomage\
-set tl=0x44 0x02
 set col=!pt!nhcolor
+set tl=0x44 0x02
+set hdr=0x0C 0x04
+set h1=0x5c 0x4
+set h2=0x60 0x4
+set h3=0x10
 
 start !pt!Wbusy "rco unpack" "Running unpack rco"  /marquee 
 
@@ -10,15 +14,26 @@ for %%i in (rcofile\*.rco) do (
 if not exist %%~pi%%~ni md %%~pi%%~ni
 if not exist %%~pi%%~ni\txt md %%~pi%%~ni\txt
 if not exist %%~pi%%~ni\img md %%~pi%%~ni\img
-%pt%rcomage.exe dump %%i %%~dpi%%~ni.xml --RESDIR %%~dpi%%~ni --text txt --images img
 set ld=%%~dpi%%~ni
 set ln=%%~ni
-)
-
+set /a n1=0
+for /f "usebackq" %%h in (`!pt!sfk hexdump -pure -nofile -rawname -offlen !hdr! %%i`) do set hc=%%h
+if !hc!==00000000 set opt=none
+if !hc!==00000010 set opt=zlib
+if !hc!==00000020 set opt=rlz
+echo !opt! > !ld!\!ln!-conf.txt
+!pt!rcomage.exe dump %%i !ld!.xml --RESDIR !ld! --text txt --images img
 if exist !ld!\!ln!-list.txt del /Q !ld!\!ln!-list.txt
 
-for %%f in (%ld%\img\*.gim) do (
-for /f "usebackq" %%a in (`%pt%sfk hexdump -pure -nofile -rawname -offlen %tl% %%f`) do set np=%%a
+for /f "tokens=*" %%m in ('CertUtil -hashfile %%i MD5') do (
+set /a n1+=1 && if !n1!==2 set hash=%%m && set MD5=!hash: =!
+)
+echo %%i !MD5! > %%i.md5
+
+for %%f in (!ld!\img\*.gim) do (
+echo check %%f 
+set /a rez=0
+for /f "usebackq" %%a in (`!pt!sfk hexdump -pure -nofile -rawname -offlen !tl! %%f`) do set np=%%a
 if !np!==0000 set opt=-ps3rgba5650
 if !np!==0001 set opt=-ps3rgba5551
 if !np!==0002 set opt=-ps3rgba4444
@@ -33,9 +48,25 @@ if !np!==000A set opt=-ps3dxt5
 if !np!==0108 set opt=-ps3dxt1ext
 if !np!==0109 set opt=-ps3dxt3ext
 if !np!==010A set opt=-ps3dxt5ext
-echo %%~nf !opt! >> !ld!\!ln!-list.txt
-GimConv\GimConv.exe %%f -o !ld!\img\%%~nf.png 
-del /Q %%f
+for /f "usebackq" %%j in (`!pt!sfk hexdump -pure -nofile -rawname -offlen !h1! %%f`) do set hc1=%%j
+for /f "usebackq" %%g in (`!pt!sfk hexdump -pure -nofile -rawname -offlen !h2! %%f`) do set hc2=%%g
+set /a rez=0x!hc1!+0x!hc2!+!h3!
+for %%l in ("%%f") do set size=%%~zl
+IF !rez! LEQ !size! (
+!pt!sfk partcopy %%f -quiet -allfrom -yes !rez! !ld!\%%~nf.vtxt
+!pt!sfk rep !ld!\%%~nf.vtxt -bin /00/0D0A/ -yes -quiet
+set /a c=0
+for /f "UseBackQ Delims=" %%V IN (!ld!\%%~nf.vtxt) do (
+set /a c+=1
+if !c!==4 set "cver=%%V"
 )
+echo !cver!> !ld!\%%~nf.vtxt
+)
+echo %%~nf !opt!>> !ld!\!ln!-list.txt
+GimConv\GimConv.exe %%f -o !ld!\img\%%~nf.png 
+)
+)
+rem del /Q /S rcofile\*.gim
+
 echo Finished. |!col! 0A
 !pt!Wbusy "rco unpack" "Done" /Stop /sound /timeout:1
--- pack.bat
+++ pack.bat
@@ -6,23 +6,41 @@ set col=!pt!nhcolor
 if not exist new_rco md new_rco
 set nr=new_rco
 
+start !pt!Wbusy "rco pack" "Running pack rco"  /marquee 
 for %%i in (rcofile\*.rco) do (
+set /a n1=0
 set xm=%%~dpi%%~ni
 set rco=!nr!\%%~ni.rco
 set ln=%%~ni
-)
-
-start !pt!Wbusy "rco pack" "Running pack rco"  /marquee 
 
 for /f "tokens=1,2" %%a in (!xm!\!ln!-list.txt) do (
-GimConv\GimConv.exe %xm%\img\%%a.png -o %xm%\img\%%a.gim %%b
-rem del /Q !xm!\img\%%a.png
+if exist !xm!\%%a.vtxt (
+for /f "tokens=1,2" %%c in (!xm!\%%a.vtxt) do (
+set g1=%%c
+if !g1!==GimConv (set g2=%%d
+set g2=!g2:~4,1!
+set "op=--update_fileinfo on"
+)
+)
+) else (
+set g2=e
+set "op="
+)
+GimConv\GimConv!g2! !xm!\img\%%a.png -o !xm!\img\%%a.gim %%b !op!
 )
 echo Wait - compile |!col! 0A
 
-!pt!rcomage.exe compile !xm!.xml !rco! --pack-hdr zlib
+for /f "tokens=1" %%h in (!xm!\!ln!-conf.txt) do (
+!pt!rcomage.exe compile !xm!.xml !rco! --pack-hdr %%h
+)
 
+for /f "tokens=*" %%m in ('CertUtil -hashfile !rco! MD5') do (
+set /a n1+=1 && if !n1!==2 set hash=%%m && set MD5=!hash: =!
+)
+echo !rco! !MD5! > !rco!.md5
+)
 !pt!Wbusy "rco pack" "Done" /Stop /sound /timeout:1
 
 echo.
 echo Finished. |!col! 0A
+:end
